32c8b353771222d756f8582e5a44b78cf8d52ee1
sahilbheke <sahil.bheke@joshsoftware.com>
2025-02-13 15:28:22 +0530

    add assignment day5

--- Day5/1.js
+++ Day5/1.js
@@ -1,13 +1,30 @@
-function validate(input){
-    const value = document.getElementById('input').value
-    if (value.length == 0 ){
-        alert("Required")
-    }
-    else if(value.length <= 1 || value.length > 10){
-        alert("False")
-    }else{
-        alert("True")
-    }
+function validate() {
+
+  const value = document.getElementById('input').value
+  const validation = new MyYup()
+
+  if(!validation.required(value)){
+    alert("Required")
+  }else if(!validation.min(value)){
+    alert("False, input length shoulb be greater than 2!")
+  }else if(!validation.max(value)){
+    alert("False, input length shoulb be smaller than 11!")
+  }else{
+    alert("True")
+  }
 }
 
+class MyYup {
+
+  max(value) {
+    return value.length > 10 ? 0 : 1
+  }
+  min(value) {
+    return value.length <= 2 ? 0 : 1
+  }
+  required(value) {
+    return (!value || value.length == 0) ? 0 : 1
+  }
+
+}
 
