d8f46045c168438e0d29e132ec7ddfee10873e20
sahilbheke <sahil.bheke@joshsoftware.com>
2025-02-13 16:39:07 +0530

    add isValid fucntin in 1.js

--- Day5/1.js
+++ Day5/1.js
@@ -2,16 +2,7 @@ function validate() {
 
   const value = document.getElementById('input').value
   const validation = new MyYup()
-
-  if(!validation.required(value)){
-    alert("Required")
-  }else if(!validation.min(value)){
-    alert("False, input length shoulb be greater than 2!")
-  }else if(!validation.max(value)){
-    alert("False, input length shoulb be smaller than 11!")
-  }else{
-    alert("True")
-  }
+  validation.isValid(value)
 }
 
 class MyYup {
@@ -26,5 +17,16 @@ class MyYup {
     return (!value || value.length == 0) ? 0 : 1
   }
   
+  isValid(value){
+    if(!validation.required(value)){
+      alert("Required")
+    }else if(!validation.min(value)){
+      alert("False, input length shoulb be greater than 2!")
+    }else if(!validation.max(value)){
+      alert("False, input length shoulb be smaller than 11!")
+    }else{
+      alert("True")
+    }
+  }
 }
 
